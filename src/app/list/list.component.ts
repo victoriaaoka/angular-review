d21a5dbf99ae84df66c98fed996f8022fae78db4
Victoria <victoriaaoka@gmail.com>
2020-03-08 19:36:00 +0300

    Use the http service and subscribe

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  brews: object;
+
+  constructor(private _http: HttpService) { }
 
   ngOnInit(): void {
+    this._http.getData().subscribe(data => {
+      this.brews = data;
+      console.log(this.brews);
+    });
   }
 
 }
