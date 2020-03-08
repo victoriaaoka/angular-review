4975b15616d96f42d16a0af9d12b8afae54302ee
Victoria <victoriaaoka@gmail.com>
2020-03-08 18:47:06 +0300

    just changed value of condition from 4 to 5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,8 +21,8 @@ export class HomeComponent implements OnInit {
 
   setClasses() {
     let myClasses = {
-      active: this.clickCounter > 4,
-      notactive: this.clickCounter <= 4
+      active: this.clickCounter > 5,
+      notactive: this.clickCounter <= 5
     };
     return myClasses;
   }
