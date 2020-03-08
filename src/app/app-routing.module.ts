b126308a255f57333671b98d20088b2c41da524d
Victoria <victoriaaoka@gmail.com>
2020-03-08 16:59:45 +0300

    Add routing for home and list components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'list', component: ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
