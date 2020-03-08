fff52b28a1b23bc47338a9302086f2db2089a450
Victoria <victoriaaoka@gmail.com>
2020-03-08 19:35:25 +0300

    Create the http service and add a getData method

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+  getData(){
+    return this.http.get('https://api.openbrewerydb.org/breweries');
+  }
+
 }
